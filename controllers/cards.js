a66ee86839d5c19414148817482c39c00beadea9
Arina Pristupa <pristupa.arina2018@yandex.by>
2023-07-05 18:01:03 +0300

    пр14 правки

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,6 +4,7 @@ const ForbiddenError = require('../errors/ForbiddenError');
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
+    .populate('likes')
     .then((card) => res.status(200).send(card))
     .catch(next);
 };
