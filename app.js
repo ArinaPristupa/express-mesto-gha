16ab2463fc239f5610c3b27aa37f6f826ad8faff
Arina Pristupa <pristupa.arina2018@yandex.by>
2023-07-05 14:34:47 +0300

    пр14

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const handleError = require('./middlewares/handleError');
 
 const NotFoundError = require('./errors/NotFoundError');
 
-const app = express();
-
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+const app = express();
 
 app.use(express.json());
 
@@ -23,20 +23,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.post('/signup', validationCreateUser, createUser);
+app.post('/signin', validationLogin, login);
+
+app.use(auth);
+
 app.use('/', router.all('*', (req, res, next) => {
   next(new NotFoundError('404 Ошибка! Данные не найдены!'));
 }));
 
-app.use(helmet());
-app.use(errors());
-app.use(handleError);
-
-app.post('/signup', validationCreateUser, createUser);
-app.post('/signin', validationLogin, login);
-
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use(auth);
+app.use(errors());
+
+app.use(helmet());
+
+app.use(handleError);
 
 app.listen(3000);
